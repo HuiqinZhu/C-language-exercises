a269b075beda55ead1d456e22b0e7ceb5df77804
Huiqin Zhu <myqf66@gmail.com>
2020-07-23 13:49:36 +0900

    Update CS50.c

--- CS50.c
+++ CS50.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <cs50.h>
+
 
 int main(void)
 {
 printf("hello, world\n");
+ 
  
 int counter = 0;
 while (true)
@@ -11,6 +14,8 @@ printf("%i\n", counter);
 counter++;
 } 
  
+int x = 10;
+int y = 11;
 if (x < y)
  {
  printf("x is less than y\n");
@@ -24,10 +29,44 @@ if (x < y)
  printf("x is equal to y\n");
  } 
 
+if ( X>=0 && X<=10)
+ {
+ printf("x is a number between 0 and 10\n");
+ }
+ if ( y>11 || y<11)
+ {
+ printf("y is not 11\n");
+ } 
+ 
 string name;
 name = GetString();
 printf("hello, Koto\n"); 
 
+string name1;
+printf("What is your name?");
+name1 = GetString();
+printf("how are you, %s\n", name1); 
+ 
+ 
+while (true) 
+ {
+printf("I am a buggy program");
+}  
+ 
+for (int i = 0; i < 100; i++)
+{
+printf("I can count to %i\n", i);
+} 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 } 
  
 
