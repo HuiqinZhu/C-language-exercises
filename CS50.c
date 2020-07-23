fd66170da8c997b2a9f8f3c0fab7bf851ed967a5
Huiqin Zhu <myqf66@gmail.com>
2020-07-23 13:37:42 +0900

    Update CS50.c

--- CS50.c
+++ CS50.c
@@ -1,13 +1,41 @@
+#include <stdio.h>
+
 int main(void)
 {
 printf("hello, world\n");
- } 
  
-
-
 int counter = 0;
 while (true)
 {
 printf("%i\n", counter);
 counter++;
 } 
+ 
+if (x < y)
+ {
+ printf("x is less than y\n");
+ }
+ else if (x > y)
+ {
+ printf("x is greater than y\n");
+ }
+ else
+ {
+ printf("x is equal to y\n");
+ } 
+
+string name;
+name = GetString();
+printf("hello, Koto\n"); 
+
+} 
+ 
+
+
+
+
+
+
+
+
+ 
