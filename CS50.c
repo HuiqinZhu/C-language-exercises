90d90dca159a83daf9823158c6226b393a8886d6
Huiqin Zhu <myqf66@gmail.com>
2020-07-23 14:13:51 +0900

    Update CS50.c

--- CS50.c
+++ CS50.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include <cs50.h>
 
+void cough(int n)
+
 
 int main(void)
 {
 printf("hello, world\n");
- 
- 
-int counter = 0;
-while (true)
-{
-printf("%i\n", counter);
-counter++;
-} 
  
 int x = 10;
 int y = 11;
@@ -39,19 +33,56 @@ if ( X>=0 && X<=10)
  } 
  
 string name;
-name = GetString();
+name = get_string();
 printf("hello, Koto\n"); 
 
 string name1;
 printf("What is your name?");
-name1 = GetString();
-printf("how are you, %s\n", name1); 
+name1 = get_string();
+printf("how are you, %s?\n", name1); 
+printf("Your lucky letter is %c\n", get_char());
+printf("Your lucky number is %i\n", get_int());
+printf("Your lucky fraction number is %f\n", get_float());
+printf("Your lucky fraction number is %d\n", get_double());
+printf("Your lucky word is %s\n", get_string()); 
+ //get_char() get_string() get_int() get_long() get_float() get_double() 
+ //%s %c %i %li %f
+ 
+printf("you are at least %i days old", get_string("what's your age\n"*365));
+ 
+
+int n; 
+if( n%2 == 0) 
+{
+printf("odd\n");
+}
+ else
+{
+ printf("even\n");
+} 
  
  
+ 
+ 
+int counter = 0;
+while (true)
+{
+printf("%i\n", counter);
+counter++;
+}
+  
 while (true) 
  {
 printf("I am a buggy program");
-}  
+}
+ 
+do
+ {
+ printf(x);
+}
+ while (x<11);  
+ 
+ 
  
 for (int i = 0; i < 100; i++)
 {
@@ -59,7 +90,7 @@ printf("I can count to %i\n", i);
 } 
  
  
- 
+cough(3); 
  
  
  
@@ -70,6 +101,14 @@ printf("I can count to %i\n", i);
 } 
  
 
+//to make a void parameterized below the main void
+void cough(int n)
+{
+for(int i = 0; i <n; i++)
+{
+printf("cough\n");
+}
+}
 
 
 
