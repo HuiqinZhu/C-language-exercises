2bcc900d5d239105358a53fbca61ae2b6657c602
Huiqin Zhu <myqf66@gmail.com>
2020-07-27 00:00:02 +0900

    Update CS50_4.c

--- CS50_4.c
+++ CS50_4.c
@@ -1,1 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include "structs.h"
+// number of students
 
+int main(void) 
+{ 
+// declare students
+student students[STUDENTS]; 
+  // populate students with user's input
+ for (int i = 0; i < STUDENTS; i++) 
+ { 
+printf("Student's name: "); 
+students[i].name = GetString(); 
+printf("Student's house: "); 
+students[i].house = GetString(); 
+} 
+// now print students
+ for (int i = 0; i < STUDENTS; i++) 
+{ 
+printf("%s is in %s.\n", students[i].name, students[i].house); 
+} 
+// free memory
+for (int i = 0; i < STUDENTS; i++) 
+{ 
+free(students[i].name); 
+free(students[i].house); 
+}  
+}
