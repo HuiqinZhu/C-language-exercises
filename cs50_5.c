97b43a1e70d660cd6a02c930e69783c61a6f107e
Huiqin Zhu <myqf66@gmail.com>
2020-07-27 00:06:17 +0900

    Create cs50_5.c

---
+++ cs50_5.c
@@ -0,0 +1,26 @@
+
+ typedef struct node 
+{ 
+int n; 
+struct node *next; 
+} 
+node;
+
+
+
+
+
+
+bool search(int n, node* list) 
+ { 
+node* ptr = list; 
+while (ptr != NULL) 
+{ 
+if (ptr->n == n) 
+{ 
+return true; 
+} 
+ptr = ptr->next; 
+} 
+return false; 
+}
